cae5b1878d4e10496c24031e309ca43437f31170
ijinxin <874450097@qq.com>
2018-08-14 23:53:19 +0800

    头部结构微调，样式todo

--- src/components/manage-header/manage-header.vue
+++ src/components/manage-header/manage-header.vue
@@ -1,19 +1,19 @@
 <template>
 <div class="manage-header-inner">
-  <section class="header-left">
-    <img src=""/>
-    <div class="userInfo">
-      <h4>{{userInfo.name}}</h4>
-      <ul>
-        <li><i></i>{{userInfo.introduction}}</li>
-        <li><i class="sky icon-man"></i>{{userInfo.sex}}</li>
-        <li><i></i>{{userInfo.email}}</li>
-      </ul>
-    </div>
-  </section>
-  <section class="header-right">
-    <el-button type="primary">编辑资料</el-button>
-  </section>
+  <div class="user-logo">
+    <img src="../../../static/img/admin.jpg" style="width: 120px"/>
+  </div>
+  <div class="user-info">
+    <h1>
+      <span>{{userInfo.name}}</span>
+      <i></i>
+    </h1>
+    <ul class="detail">
+      <li><i class="sky icon-introduction"></i>{{userInfo.introduction}}</li>
+      <li><i class="sky" :class="{'icon-man':userInfo.sex === 'man', 'icon-women':userInfo.sex === 'women', 'icon-secret':userInfo.sex === 'secret'}"></i></li>
+      <li><i class="sky icon-email"></i>{{userInfo.email}}</li>
+    </ul>
+  </div>
 </div>
 </template>
 
@@ -26,7 +26,7 @@ export default {
         img: '',
         name: '管理员',
         introduction: '管理员的自我介绍',
-        sex: '男',
+        sex: 'man',
         email: 'admin.qq.com'
       }
     }
@@ -39,10 +39,17 @@ export default {
   width 1000px
   margin 0 auto
   height 100%
-  .header-left
-    width 850px
-    height 100%
-  .header-right
+  display flex
+  justify-content space-between
+  .user-logo
     width 150px
-    height 100%
+    display flex
+    justify-content center
+    align-items center
+  .user-info
+    width 830px
+    h1
+      line-height 30px
+    .detail
+      list-style none
 </style>
