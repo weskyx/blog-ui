5df2312db6d32a8a40c18276c40220529fea5480
ijinxin <874450097@qq.com>
2018-08-18 15:07:00 +0800

    mavonEditor配合图片上传下载实现md文件编辑和展示

--- src/views/manage/article/manage-article.vue
+++ src/views/manage/article/manage-article.vue
@@ -1,10 +1,16 @@
 <template>
 <div class="article-editor">
-  <mavon-editor v-model="article" :boxShadow="false"></mavon-editor>
+  <mavon-editor ref=md v-model="article" :boxShadow="false"
+                @imgAdd="addImg" @imgDel="delImg"></mavon-editor>
+  <div class="test-article">
+    <mavon-editor ref=md v-model="article" :toolbarsFlag="false" :editable="false" :subfield="false"></mavon-editor>
+  </div>
 </div>
 </template>
 
 <script>
+import $http from '@/services/http'
+// import {mavonEditor} from ...
 export default {
   name: 'manageArticle',
   data () {
@@ -17,6 +23,18 @@ export default {
   methods: {
     getData () {
       console.log(this.article)
+    },
+    addImg (pos, $file) {
+      // 第一步.将图片上传到服务器.
+      let formdata = new FormData()
+      formdata.append('image', $file)
+      $http.post('server url', null, formdata).then(url => {
+        // 将md中的图片名更换为url
+        this.$refs.md.$img2Url(pos, url)
+      })
+    },
+    delImg () {
+      // delete
     }
   }
 }
@@ -26,6 +44,7 @@ export default {
 .article-editor
   flex 1
   display flex
+  flex-direction column
   .v-note-wrapper.markdown-body
     flex 1 !important
   .v-right-item
