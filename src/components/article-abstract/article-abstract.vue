2c9cde8b6a6fb70b93ea2eeb7066c9c4ad57995c
ijinxin <874450097@qq.com>
2018-08-20 20:35:46 +0800

    article摘要

---
+++ src/components/article-abstract/article-abstract.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="article-abstract">
+  <header>
+    <img src="@/../static/img/thumb.gif"/>
+    <div class="article-info">
+      <p class="title">{{title}}</p>
+      <p class="author">{{author}} / {{postedTime}}</p>
+    </div>
+  </header>
+  <section>
+    {{content}}...<a>阅读全文</a>
+  </section>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'article-abstract',
+  props: ['articleInfo'],
+  data () {
+    return {
+      userLogo: this.articleInfo.userLogo,
+      title: this.articleInfo.title,
+      author: this.articleInfo.author,
+      postedTime: this.articleInfo.postedTime,
+      content: this.articleInfo.content.trim().toLowerCase()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
